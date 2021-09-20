e402b6d9b602d72f0a82db7823c48fdcf492606a
Tijl Deneut <deneut_tijl@hotmail.com>
2021-09-20 18:36:38 +0200

    Add files via upload
    v 0.3.0, added RSA decryption

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dpapick3",
-    version="0.2.0",
+    version="0.3.0",
     author="Tijl Deneut",
     author_email="tijl.deneut@howest.be",
     description="A native implementation of DPAPI",
